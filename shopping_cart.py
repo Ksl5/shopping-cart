7c113ca6f1e93d62c7447ed741f69fdbe80f1139
Ksl5 <65416583+Kiranlutfeali@users.noreply.github.com>
2020-05-29 20:58:31 -0400

    Look up product given id

--- shopping_cart.py
+++ shopping_cart.py
@@ -39,12 +39,16 @@ def to_usd(my_price):
 
 #information capture / input
 
-selected_id = input("Please input a product identifier:")
-print(selected_id)
-print(type(selected_id))
+selected_id = input("Please input a product identifier: ")   #string
+matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+matching_product = matching_products[0]
+print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
+
+#print(matching_product)
+#print(type(matching_product))
 
 
 
 
 
-#information output
+#information display / output
