2ef4d31c9bf3f320a441ed15bd1de076970948ac
Ksl5 <65416583+Kiranlutfeali@users.noreply.github.com>
2020-05-29 21:05:52 -0400

    DONE keyword to break

--- shopping_cart.py
+++ shopping_cart.py
@@ -39,10 +39,21 @@ def to_usd(my_price):
 
 #information capture / input
 
-selected_id = input("Please input a product identifier: ")   #string
-matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
-matching_product = matching_products[0]
-print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
+while True:
+    selected_id = input("Please input a product identifier: ")   #string
+    if selected_id == "DONE":
+        break
+    else:    
+        matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+        matching_product = matching_products[0]
+        print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
+
+    
+
+
+
+
+
 
 #print(matching_product)
 #print(type(matching_product))
